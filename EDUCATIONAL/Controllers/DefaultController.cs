4c5d23bc0aa2e4479af7631483a4dd4b5c810ec1
Dhana Chinta <chdhanalakshmi@outlook.com>
2019-06-28 11:48:55 -0500

    moving code to git

---
+++ EDUCATIONAL/Controllers/DefaultController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EDUCATIONAL.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        public ActionResult BLOG()
+        {
+            return View();
+        }
+        public ActionResult EVENTS()
+        {
+            return View();
+        }
+        public ActionResult GALLERY()
+        {
+            return View();
+        }
+        public ActionResult MANAGEMENT()
+        {
+            return View();
+        }
+        
+    }
+}
---
+++ EDUCATIONAL/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EDUCATIONAL.MvcApplication" Language="C#" %>
